2cf320ccc01fc739e6f1a02af2192bd4d7f45e4e
Sachin Koti <sachinkoti123@gmail.com>
2023-01-29 18:13:54 -0800

    Add files via upload

---
+++ face_utils.py
@@ -0,0 +1,257 @@
+import numpy as np
+from collections import OrderedDict
+import cv2
+
+def rect_to_bb(rect):
+    # take a bounding predicted by dlib and convert it
+    # to the format (x, y, w, h) as we would normally do
+    # with OpenCV
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
+    
+    # return a tuple of (x, y, w, h)
+    return (x, y, w, h)
+
+def shape_to_np(shape, dtype="int"):
+    # initialize the list of (x, y)-coordinates
+    coords = np.zeros((68, 2), dtype = dtype)
+    
+    # loop over the 68 facial landmarks and convert them
+    # to a 2-tuple of (x, y) coordinates
+    for i in range(0, 68):
+            coords[i] = (shape.part(i).x, shape.part(i).y)
+        
+    # return the list of (x,y)-coordinates
+    return coords
+
+# define a dictionary that maps the indexes of the facial 
+# landmarks to specific face regions
+FACIAL_LANDMARKS_IDXS = OrderedDict([
+    ("mouth", (48, 68)),
+    ("right_eyebrow", (17, 22)),
+    ("left_eyebrow", (22, 27)),
+    ("right_eye", (36, 42)),
+    ("left_eye", (42, 48)),
+    ("nose", (27, 36)),
+    ("jaw", (0, 17))
+])
+
+def visualize_facial_landmarks(image, shape, colors = None, alpha = 0.75):
+    # create two copies of the input image -- one for the
+    # overlay and one for the final output image
+    overlay = image.copy()
+    output = image.copy()
+    
+    # if the colors list is None, initialize it with a unique
+    # color for each facial landmark region
+    if colors is None:
+        colors = [(19, 199, 109), (79, 76, 240), (230, 159, 23),
+            (168, 100, 168), (158, 163, 32),
+            (163, 38, 32), (180, 42, 220)]
+    
+    # loop over the facial landmark regions individually
+    for (i, name) in enumerate(FACIAL_LANDMARKS_IDXS.keys()):
+        # grab the (x,y)-coordinates associated with the
+        # face landmark
+        (j, k) = FACIAL_LANDMARKS_IDXS[name]
+        pts = shape[j : k]
+
+        # check if supposed to draw the jawline
+        if name == "jaw":
+            # since the jawline is a non-enclosed facial region,
+            # just draw lines between the (x, y) - coordinates
+            for l in range(1, len(pts)):
+                ptA = tuple(pts[l - 1])
+                ptB = tuple(pts[l])
+                cv2.line(overlay, ptA, ptB, colors[i], 2)
+
+        else:
+            hull = cv2.convexHull(pts)
+            cv2.drawContours(overlay, [hull], -1, colors[i], -1)
+            
+    # apply the transparent overlay
+    cv2.addWeighted(overlay, alpha, output, 1-alpha, 0, output)
+
+    # return the output image
+    return output
+
+
+# import the necessary packages
+from threading import Thread
+import cv2
+
+class WebcamVideoStream:
+    def __init__(self, src=0, name="WebcamVideoStream"):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
+
+        # initialize the thread name
+        self.name = name
+
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        t = Thread(target=self.update, name=self.name, args=())
+        t.daemon = True
+        t.start()
+        return self
+
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                return
+
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        # return the frame most recently read
+        return self.frame
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
+
+class VideoStream:
+    def __init__(self, src=0, usePiCamera=False, resolution=(320, 240),
+        framerate=32, **kwargs):
+        # check to see if the picamera module should be used
+        if usePiCamera:
+            # only import the picamera packages unless we are
+            # explicity told to do so -- this helps remove the
+            # requirement of `picamera[array]` from desktops or
+            # laptops that still want to use the `imutils` package
+            from .pivideostream import PiVideoStream
+
+            # initialize the picamera stream and allow the camera
+            # sensor to warmup
+            self.stream = PiVideoStream(resolution=resolution,
+                framerate=framerate, **kwargs)
+
+        # otherwise, we are using OpenCV so initialize the webcam
+        # stream
+        else:
+            self.stream = WebcamVideoStream(src=src)
+
+    def start(self):
+        # start the threaded video stream
+        return self.stream.start()
+
+    def update(self):
+        # grab the next frame from the stream
+        self.stream.update()
+
+    def read(self):
+        # return the current frame
+        return self.stream.read()
+
+    def stop(self):
+        # stop the thread and release any resources
+        self.stream.stop()
+
+
+import sys
+import time
+
+# import the Queue class from Python 3
+if sys.version_info >= (3, 0):
+    from queue import Queue
+
+# otherwise, import the Queue class for Python 2.7
+else:
+    from Queue import Queue
+
+    
+class FileVideoStream:
+    def __init__(self, path, transform=None, queue_size=128):
+        # initialize the file video stream along with the boolean
+        # used to indicate if the thread should be stopped or not
+        self.stream = cv2.VideoCapture(path)
+        self.stopped = False
+        self.transform = transform
+
+        # initialize the queue used to store frames read from
+        # the video file
+        self.Q = Queue(maxsize=queue_size)
+        # intialize thread
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+
+    def start(self):
+        # start a thread to read frames from the file video stream
+        self.thread.start()
+        return self
+
+    def update(self):
+        # keep looping infinitely
+        while True:
+            # if the thread indicator variable is set, stop the
+            # thread
+            if self.stopped:
+                break
+
+            # otherwise, ensure the queue has room in it
+            if not self.Q.full():
+                # read the next frame from the file
+                (grabbed, frame) = self.stream.read()
+
+                # if the `grabbed` boolean is `False`, then we have
+                # reached the end of the video file
+                if not grabbed:
+                    self.stopped = True
+
+                # if there are transforms to be done, might as well
+                # do them on producer thread before handing back to
+                # consumer thread. ie. Usually the producer is so far
+                # ahead of consumer that we have time to spare.
+                #
+                # Python is not parallel but the transform operations
+                # are usually OpenCV native so release the GIL.
+                #
+                # Really just trying to avoid spinning up additional
+                # native threads and overheads of additional
+                # producer/consumer queues since this one was generally
+                # idle grabbing frames.
+                if self.transform:
+                    frame = self.transform(frame)
+
+                # add the frame to the queue
+                self.Q.put(frame)
+            else:
+                time.sleep(0.1)  # Rest for 10ms, we have a full queue
+
+        self.stream.release()
+
+    def read(self):
+        # return next frame in the queue
+        return self.Q.get()
+
+    # Insufficient to have consumer use while(more()) which does
+    # not take into account if the producer has reached end of
+    # file stream.
+    def running(self):
+        return self.more() or not self.stopped
+
+    def more(self):
+        # return True if there are still frames in the queue. If stream is not stopped, try to wait a moment
+        tries = 0
+        while self.Q.qsize() == 0 and not self.stopped and tries < 5:
+            time.sleep(0.1)
+            tries += 1
+
+        return self.Q.qsize() > 0
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
+        # wait until stream resources are released (producer thread might be still grabbing frame)
+        self.thread.join()
